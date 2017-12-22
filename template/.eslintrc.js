75acb6eb5034c5ca502383c90e67bdfbd8ad6e00
SoftProDev <softprodev15@gmail.com>
2017-12-22 16:01:06 +0100

    Fix typo

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -1,17 +1,16 @@
 module.exports = {
   root: true,
+  parser: 'babel-eslint',
   env: {
     browser: true,
     node: true
   },
-  extends: [
-    'standard',
-    'plugin:vue/recommended'
+  extends: 'standard',
+  // required to lint *.vue files
+  plugins: [
+    'html'
   ],
-
   // add your custom rules here
   rules: {},
-  globals: {
-    Vue: true
-  }
+  globals: {}
 }
--- template/pages/index.vue
+++ template/pages/index.vue
@@ -9,14 +9,8 @@
         {{ description }}
       </h2>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
+        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
       </div>
     </div>
   </section>
