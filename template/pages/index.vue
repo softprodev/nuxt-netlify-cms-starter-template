1b3aedf21ff084d2a326a38cc30e1e4dfde60c8a
SoftProDev <softprodev15@gmail.com>
2018-01-16 10:34:08 +0100

    Rename along vue style guide

--- template/pages/index.vue
+++ template/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="container">
     <div>
-      <logo/>
+      <app-logo/>
       <h1 class="title">
         {{ name }}
       </h1>
@@ -9,19 +9,24 @@
         {{ description }}
       </h2>
       <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
+        <a href="https://nuxtjs.org/"
+          target="_blank"
+          class="button--green">Documentation</a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          class="button--grey">GitHub</a>
       </div>
     </div>
   </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import AppLogo from '~/components/AppLogo.vue'
 
 export default {
   components: {
-    Logo
+    AppLogo
   }
 }
 </script>
