038b2aef0982c8f07e142f47a2a705f6f8c7f51a
SoftProDev <softprodev15@gmail.com>
2018-03-04 15:47:28 +0100

    fix(setup): skip interpolation of Netlify CMS config

--- meta.js
+++ meta.js
@@ -1,7 +1,7 @@
 module.exports = {
   helpers: {
-    escape: function(value) {
-      return value.replace(/'/g, '&apos;');
+    raw: function (options) {
+      return options.fn(this)
     }
   },
   prompts: {
@@ -14,12 +14,17 @@ module.exports = {
       'type': 'string',
       'required': false,
       'message': 'Project description',
-      'default': 'Nuxt.js project'
+      'default': 'Nuxt.js + Netlify CMS project'
     },
     author: {
       'type': 'string',
       'message': 'Author'
     },
   },
+  skipInterpolation: [
+    "**/*.vue",
+    "app.html",
+    "static/admin/config.yml"
+  ],
   completeMessage: '{{#inPlace}}To get started:\n\n  npm install # Or yarn\n  npm run dev{{else}}To get started:\n\n  cd {{destDirName}}\n  npm install # Or yarn\n  npm run dev{{/inPlace}}'
 };
--- template/nuxt.config.js
+++ template/nuxt.config.js
@@ -1,3 +1,14 @@
+var glob = require('glob');
+var path = require('path');
+
+// Enhance Nuxt's generate process by gathering all content files from Netifly CMS
+// automatically and match it to the path of your Nuxt routes.
+// The Nuxt routes are generate by Nuxt automatically based on the pages folder.
+var dynamicRoutes = getDynamicPaths({
+  '/blog': 'blog/posts/*.json'
+});
+
+
 module.exports = {
   /*
   ** Headers of the page
@@ -7,7 +18,7 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '{{escape description }}' }
+      { hid: 'description', name: 'description', content: '{{ description }}' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
@@ -18,13 +29,19 @@ module.exports = {
   */
   loading: { color: '#3B8070' },
   /*
+  ** Route config for pre-rendering
+  */
+  generate: {
+    routes: dynamicRoutes
+  },
+  /*
   ** Build configuration
   */
   build: {
     /*
     ** Run ESLint on save
     */
-    extend (config, { isDev, isClient }) {
+    extend(config, { isDev, isClient }) {
       if (isDev && isClient) {
         config.module.rules.push({
           enforce: 'pre',
@@ -35,4 +52,19 @@ module.exports = {
       }
     }
   }
+}
+
+/**
+ * Create an array of URLs from a list of files
+ * @param {*} urlFilepathTable
+ */
+function getDynamicPaths(urlFilepathTable) {
+  return [].concat(
+    ...Object.keys(urlFilepathTable).map(url => {
+      var filepathGlob = urlFilepathTable[url];
+      return glob
+        .sync(filepathGlob, { cwd: 'content' })
+        .map(filepath => `${url}/${path.basename(filepath, '.json')}`);
+    })
+  );
 }
